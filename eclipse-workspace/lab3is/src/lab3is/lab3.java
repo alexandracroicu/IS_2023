98c4ce7b9c9fb98bcf0e92bbfec830832b7d2805
Student <student@b623-14.intranet.cs.upt.ro>
2023-03-09 17:01:33 +0200

    cevreau

---
+++ eclipse-workspace/lab3is/src/lab3is/lab3.java
@@ -0,0 +1,7 @@
+package lab3is;
+
+public class lab3 {
+	public static void main(String[] args) {
+		System.out.print("hello ");
+
+}}
---
+++ eu/src/nustiuce/nustiuce2.java
@@ -0,0 +1,7 @@
+package nustiuce;
+
+public class nustiuce2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
